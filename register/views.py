c0b65b6a0e33b5f6f6601bf1f75a9d66921fd0cc
Govind R Nair <revertionist@pop-os.localdomain>
2023-05-09 23:29:25 +0530

    component update

---
+++ register/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+def signup(request):
+    form = UserCreationForm()
+    return render (request, 'register/signup.html', {"form": form})
