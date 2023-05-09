ff4746d88d0ffb6df2e84022e72b474988cbce2a
Govind R Nair <revertionist@pop-os.localdomain>
2023-05-09 22:57:06 +0530

    initial navbar

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render (request, 'main/base.html')
+    return render (request, 'main/pages/home.html')
