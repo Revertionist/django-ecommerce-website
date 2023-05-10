ba3092c8c1a05d8f323904e4466d24181cb14d88
Govind R Nair <govindrnair.rev@gmail.com>
2023-05-10 21:33:35 +0530

    product update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,4 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = "media/"
+
+MEDIA_URL = "media/"
+
 CRISPY_TEMPLATE_PACK="bootstrap4"
--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
+class Product(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField (max_length=100, null=False)
     description = models.TextField (null=False)
-    image = models.ImageField(upload_to=None)
+    image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
